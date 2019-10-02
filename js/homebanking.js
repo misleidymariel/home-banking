149c0baf9a340828fff3824fb441c8d269e6a89c
Misleidy <misleidy@MacBook-Pro-de-Misleidy.local>
2019-10-01 22:12:21 -0300

    finalizacion del home banking

--- js/homebanking.js
+++ js/homebanking.js
@@ -2,20 +2,20 @@
 var cuenta = {
     nombre: "misleidy",
     contrasena: "12345qwe",
-    amigos:[
+    amigos: [
         {
-            numeroCta:"12345600",
-            nombre:"alex ventura"
+            numeroCta: "1234560023",
+            nombre: "Alex Ventura"
 
         },
         {
-            numeroCta:"7890463",
-            nombre:"Mariely Espinoza"
+            numeroCta: "78904630078",
+            nombre: "Mariely Espinoza"
 
         },
     ]
 }
-var nombreUsuario= cuenta.nombre;
+var nombreUsuario = cuenta.nombre;
 var contrasena = "";
 var saldoCuenta = 8000;
 var limiteExtraccion = 5000;
@@ -23,35 +23,38 @@ var servicioAPagar = '';
 
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
-window.onload = function() {
+window.onload = function () {
     cargarNombreEnPantalla();
     actualizarSaldoEnPantalla();
     actualizarLimiteEnPantalla();
 }
+
 //Funcion principal que se llama desde el menu del home banking
+
 function operation(operationType) {
     document.getElementById('operation-dialog').style.display = 'block';
 
-    switch(operationType){
+    switch (operationType) {
         case "extraer": extraerDineroVista(); break;
         case "depositar": depositarDineroVista(); break;
         case "servicio": pagarServicioVista(); break;
-        case "tranferir": transferirDineroVista();break;
+        case "tranferir": transferirDineroVista(); break;
         case "limite-extracion": cambiarLimiteDeExtraccionVista(); break;
     }
 }
 
 function iniciarSesion() {
     nombreUsuario = document.getElementById('usuarioSesion').value;
-    contrasena =  document.getElementById('usuarioContrasena').value;
-    
-    if (nombreUsuario === cuenta.nombre && contrasena===cuenta.contrasena) {
+    contrasena = document.getElementById('usuarioContrasena').value;
+
+    //validacion
+    if (nombreUsuario === cuenta.nombre && contrasena === cuenta.contrasena) {
 
         swal({
-            title: "Bienvenido ",
-            text: "Bienvenido " + nombreUsuario + " ya puedes comenzar a realizar operaciones. ",
+            title: "Bienvenido/a" + " " + nombreUsuario,
+            text: "Ya puedes comenzar a realizar operaciones. ",
         }).then(() => {
-            window.location.href ="index.html";
+            window.location.href = "index.html";
 
             actualizarSaldoEnPantalla();
 
@@ -61,11 +64,12 @@ function iniciarSesion() {
     } else {
 
         swal({
-            title: "Cuenta incorrecta ",
-            text: " " + nombreUsuario + " ya puedes comenzar a realizar operaciones. ",
+            title: "Usuario o contraseña incorrecta ",
+            text: "tu dinero a sido retenido por cuestiones de seguridad",
+            icon: "error",
         });
     }
- 
+
 }
 
 function extraerDineroVista() {
@@ -91,7 +95,7 @@ function pagarServicioVista() {
 
 }
 
-function pagarServicioItemVista(){
+function pagarServicioItemVista() {
     servicioAPagar = document.querySelector('input[name="servicio"]:checked').value;
     var pagarForm = document.getElementById('pagar-template-form').innerHTML;
     document.getElementById('operation-dialog-title').innerHTML = "Pagar Servicio " + servicioAPagar;
@@ -101,7 +105,7 @@ function pagarServicioItemVista(){
 
 }
 
-function cambiarLimiteDeExtraccionVista(){
+function cambiarLimiteDeExtraccionVista() {
     document.getElementById('operation-dialog-title').innerHTML = "Cambiar Limite Extraccion";
     var cambiarLimiteForm = document.getElementById('cambiar-limite-template-form').innerHTML;
     document.getElementById('operation-dialog-body').innerHTML = cambiarLimiteForm;
@@ -110,11 +114,10 @@ function cambiarLimiteDeExtraccionVista(){
 }
 
 /*funcion que carga el template dialogo de tranferir dinero*/
-function transferirDineroVista(){
+function transferirDineroVista() {
     document.getElementById('operation-dialog-title').innerHTML = "Tranferir Dinero";
-    document.getElementById('amigos-cuenta').innerHTML = cuenta.amigos.map(it => it.nombre +" "+ it.numeroCta);
+    document.getElementById('amigos-cuenta').innerHTML = cuenta.amigos.map(it => it.nombre + " " + it.numeroCta).join("<br>");
     var transferirForm = document.getElementById('transferir-template-form').innerHTML;
-    
     document.getElementById('operation-dialog-body').innerHTML = transferirForm;
     document.getElementById('operation-acept-button').firstChild.data = "Aceptar";
     document.getElementById('operation-acept-button').onclick = transferirDinero;
@@ -124,18 +127,18 @@ function transferirDineroVista(){
 function extraerDinero() {
     var montoRetiro = document.getElementById('input-extract-amount').value;
     montoRetiro = parseInt(montoRetiro);
-    
+
     //Validaciones
-    if(montoRetiro%100 !== 0) { // Si el monto del retiro no es un billete de 100
-        swal ("Solo puedes extraer dinero en billetes de $ 100")
+    if (montoRetiro % 100 !== 0) { // Si el monto del retiro no es un billete de 100
+        swal("Solo puedes extraer dinero en billetes de $ 100")
         return;
     }
-    if(montoRetiro > saldoCuenta){
-        swal ("Saldo insuficiente");
+    if (montoRetiro > saldoCuenta) {
+        swal("Saldo insuficiente");
         return;
     }
-    if(montoRetiro > limiteExtraccion){
-        swal ("La cantidad de dinero es mayor a tu limite diario");
+    if (montoRetiro > limiteExtraccion) {
+        swal("La cantidad de dinero es mayor a tu limite diario");
         return;
     }
     //fin validaciones
@@ -150,17 +153,14 @@ function extraerDinero() {
 
     swal({
         title: "Extracion exitosa",
-        text:"Monto Extraccion: $ "  + montoRetiro + '\n' + "Saldo anterior: $ "  +  saldoAnterior   + '\n' + "Saldo actual: $ " + saldoCuenta,
+        text: "Monto Extraccion: $ " + montoRetiro + '\n' + "Saldo anterior: $ " + saldoAnterior + '\n' + "Saldo actual: $ " + saldoCuenta,
         icon: "success",
-      });
+    });
 }
 
 function depositarDinero() {
     var montoDeposito = document.getElementById('input-depositar-amount').value;
     montoDeposito = parseInt(montoDeposito);
-    saldoCuenta = saldoCuenta + montoDeposito;
-
-
     saldoAnterior = saldoCuenta;
     saldoCuenta = saldoCuenta + montoDeposito;
 
@@ -169,23 +169,26 @@ function depositarDinero() {
     closeDialog();
     swal({
         title: "Operacion exitosa",
-        text:"Monto deposito: $ "  + montoDeposito + '\n' + "Saldo anterior: $ "  +  saldoAnterior   + '\n' + "Saldo actual: $ " + saldoCuenta,
+        text: "Monto deposito: $ " + montoDeposito + '\n' + "Saldo anterior: $ " + saldoAnterior + '\n' + "Saldo actual: $ " + saldoCuenta,
         icon: "success",
-      });
+    });
 }
 
 function pagarServicio() {
     var montoServicio = document.getElementById('input-pagar-amount').value;
     montoServicio = parseInt(montoServicio);
-
-    if(montoServicio > saldoCuenta){
-         swal({
+    if (isNaN(montoServicio)) {
+        alert("error");
+        return;
+    }
+    if (montoServicio > saldoCuenta) {
+        swal({
             title: "Error de transferencia",
-            text:"No tienes disponible ese dinero",
+            text: "No tienes disponible ese dinero",
             icon: "error",
-            });
-            return;
-    };
+        });
+        return;
+    }
 
     saldoAnterior = saldoCuenta;
     saldoCuenta = saldoCuenta - montoServicio;
@@ -197,9 +200,10 @@ function pagarServicio() {
 
     swal({
         title: "Pago Exitoso",
-        text:" Servicio " +  servicioAPagar + " : " + " $ " + montoServicio  + '\n' + "Saldo anterior: $ "  +  saldoAnterior   + '\n' + "Saldo actual: $ " + saldoCuenta,
-        icon: "success",
-      });
+        text: " Servicio " + servicioAPagar + " : " + " $ " + montoServicio + '\n' + "Saldo anterior: $ " + saldoAnterior + '\n' + "Saldo actual: $ " + saldoCuenta,
+        icon: "success"
+    });
+
 }
 
 
@@ -208,42 +212,45 @@ function transferirDinero() {
     var cuentaTransferir = document.getElementById("input-tranferir-cuenta").value;
     montoTransferir = parseInt(montoTransferir);
 
-    var ctaAmigo = cuenta.amigos.filter(function(amigo){
+    //Validaciones
+    var ctaAmigo = cuenta.amigos.filter(function (amigo) {
         return amigo.numeroCta == cuentaTransferir;
     });
 
-    if(ctaAmigo.length == 0){
+
+    if (montoTransferir > saldoCuenta) {
         swal({
             title: "Error de transferencia",
-            text:"solo puedes transferir a cuentas agregadas",
+            text: "No tienes disponible ese dinero",
             icon: "error",
-          });
+        });
         return;
     }
 
-    if(montoTransferir > saldoCuenta){
+    if (ctaAmigo.length == 0) {
         swal({
             title: "Error de transferencia",
-            text:"No tienes disponible ese dinero",
+            text: "solo puedes transferir a cuentas agregadas",
             icon: "error",
-          });
+        });
         return;
     }
 
+    //operacion
     saldoAnterior = saldoCuenta;
     saldoCuenta = saldoCuenta - montoTransferir;
 
-    
+
     actualizarSaldoEnPantalla();
 
     closeDialog();
 
     swal({
         title: "Pago Exitoso",
-        text:"Monto transferir: $ "  + montoTransferir + '\n' + "transferirtes a : " + ctaAmigo[0].nombre + '\n' + "numero de cuenta: " + ctaAmigo[0].numeroCta,
+        text: "Monto transferir: $ " + montoTransferir + '\n' + "transferirtes a : " + ctaAmigo[0].nombre + '\n' + "numero de cuenta: " + ctaAmigo[0].numeroCta,
         icon: "success",
-      });
-   
+    });
+
 }
 function cambiarLimiteDeExtraccion() {
     var montoLimite = document.getElementById('input-limite-amount').value;
@@ -257,9 +264,9 @@ function cambiarLimiteDeExtraccion() {
 
     swal({
         title: "Operacion exitosa",
-        text:"Nuevo limite de extracion: $ " + montoLimite,
+        text: "Nuevo limite de extracion: $ " + montoLimite,
         icon: "success",
-      });
+    });
 
 }
 
@@ -277,6 +284,6 @@ function actualizarLimiteEnPantalla() {
 }
 
 //Funcion que cierra el dialogo
-function closeDialog(){
-    document.getElementById('operation-dialog').style.display='none'
+function closeDialog() {
+    document.getElementById('operation-dialog').style.display = 'none'
 }
